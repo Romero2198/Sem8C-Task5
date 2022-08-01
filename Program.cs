fbca34318b3ea4c56ac58b694eb3dffac92ef30a
Yung.Energy <morskojroma@gmail.com>
2022-08-01 20:53:40 +0300

    task5

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿int size = 4;
+int[,] array = new int[size, size];
+
+int value = 1;
+int i = 0;
+int j = 0;
+while (value <= size * size)
+{
+    array[i, j] = value;
+    if (i <= j + 1 && i + j < size - 1)
+        ++j;
+    else if (i < j && i + j >= size - 1)
+        ++i;
+    else if (i >= j && i + j > size - 1)
+        --j;
+    else
+        --i;
+    ++value;
+}
+PrintMatrix(array);
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
